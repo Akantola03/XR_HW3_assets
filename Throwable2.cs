7bbb13c7a372b5ee47d0db963341314ac4bdf28f
Atte <atte.kantola@student.oulu.fi>
2026-02-22 13:22:02 +0200

    Update Throwable2.cs

--- Throwable2.cs
+++ Throwable2.cs
@@ -3,33 +3,59 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class Throwable2 : MonoBehaviour
+
+public class ThrowableBall : MonoBehaviour
 {
     Rigidbody rb;
 
     // These are set by the grab script
     public bool isHeld = false;
+    public bool pendingThrow = false;
+
     public Vector3 lastHandPos;
     public Vector3 handVelocity;
+
+    Transform currentHand;
+
+    public float throwMultiplier = 1.5f;
+
 
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
     }
 
+    void Update()
+    {
+            
+    }
+
     void FixedUpdate()
     {
         if (isHeld)
         {
+            Vector3 newPos = currentHand.position; 
+            handVelocity = (newPos - lastHandPos) / Time.fixedDeltaTime; 
+            lastHandPos = newPos;
+
             // While held, disable physics
             rb.isKinematic = true;
             rb.useGravity = false;
+            rb.MovePosition(newPos); 
+            rb.MoveRotation(currentHand.rotation);
+
         }
         else
         {
             // When released, physics takes over
             rb.isKinematic = false;
             rb.useGravity = true;
+
+            if (pendingThrow) 
+            { 
+                rb.linearVelocity = handVelocity;
+                pendingThrow = false;
+            }
         }
     }
 
@@ -37,19 +63,14 @@ public class Throwable2 : MonoBehaviour
     public void OnGrab(Transform hand)
     {
         isHeld = true;
+        currentHand = hand;
         lastHandPos = hand.position;
     }
 
     // Called every frame while held
     public void OnHeldUpdate(Transform hand)
     {
-        // Compute hand velocity
-        handVelocity = (hand.position - lastHandPos) / Time.deltaTime;
-        lastHandPos = hand.position;
-
-        // Move object with hand
-        transform.position = hand.position;
-        transform.rotation = hand.rotation;
+        currentHand = hand;
     }
 
     // Called by CustomGrab when released
@@ -58,6 +79,8 @@ public class Throwable2 : MonoBehaviour
         isHeld = false;
 
         // Apply throw force
-        rb.velocity = handVelocity;
+        //rb.linearVelocity = handVelocity;
+        pendingThrow = true;
+        
     }
 }
